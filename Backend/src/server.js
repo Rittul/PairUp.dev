1053472fa075887e6604330ae576a12cb9d6197c
Rittul Raj <rittulraj@gmail.com>
2026-05-03 02:17:12 +0530

    updated policies

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -12,14 +12,14 @@ initializesocket(server);
 
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors({
-//     origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-//   credentials: true
-// }));
-
 app.use(cors({
-  origin: "*"
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://pairup-dev.onrender.com"],
+  credentials: true
 }));
+
+// app.use(cors({
+//   origin: "*"
+// }));
 require('dotenv').config();
 
 const authRouter= require("./routes/auth");
--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,1 +1,6 @@
-export const BASE_URL = "http://localhost:3000/";
+// Conditional BASE_URL based on environment
+const hostname = window.location.hostname;
+export const BASE_URL =
+  hostname === "localhost" || hostname === "127.0.0.1"
+    ? "http://localhost:3000/"
+    : "https://pairup-dev.onrender.com";
